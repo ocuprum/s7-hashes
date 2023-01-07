65bfa9339a8e07ca65febe8b18c1e7dd1748e797
Oleksandra Mednikova <olecuprum@gmail.com>
2023-01-07 19:15:06 +0200

    Updated attacks funcs

--- attacks/birthday_attack.py
+++ attacks/birthday_attack.py
@@ -1,13 +1,18 @@
 import random
 import string
 
+chars = list(string.printable)
+chars.remove(',')
+chars.remove('\x0b')
+chars.remove('\x0c')
+
 def ba1(message, message_hash, hash_func, size=8):
-    
     if len(message_hash) < size - 1:
         return False
     
+    messages, hashes_lst = [], []
     hashes = {}
-    number = 1
+    number = 0
 
     new_hash = message_hash[-size:]
     hashes[new_hash] = message
@@ -15,12 +20,16 @@ def ba1(message, message_hash, hash_func, size=8):
     while True:
         number += 1
         new_message = message + str(number)
-        new_hash = hash_func(new_message)[-size:]
+        messages.append(new_message)
+
+        nhash = hash_func(new_message)
+        hashes_lst.append(nhash)
+        new_hash = nhash[-size:]
 
         hash_preimage = hashes.get(new_hash, [])
         
         if hash_preimage:
-            return hash_preimage + [new_message]
+            return number, messages + [hash_preimage + [new_message]], hashes_lst
 
         hashes[new_hash] = hash_preimage + [new_message]
 
@@ -28,19 +37,26 @@ def ba2(message, message_hash, hash_func, size=8):
     if len(message_hash) < size - 1:
         return False
     
+    messages, hashes_lst = [], []
     hashes = {}
 
     new_hash = message_hash[-size:]
     hashes[new_hash] = {message}
 
+    count = 0
     while True:
         pos = random.randint(0, len(message)-1)
         new_sym = random.choice(string.printable)
         message = message[:pos] + new_sym + message[pos+1:]
-        new_hash = hash_func(message)[-size:]
+        messages.append(message)
+
+        nhash = hash_func(message)
+        hashes_lst.append(nhash)
+        new_hash = nhash[-size:]
 
         hashes[new_hash] = hashes.get(new_hash, set()) | {message}
         
+        count += 1
+
         if len(hashes[new_hash]) == 2:
-            return hashes[new_hash]
-    
+            return count, messages + [hashes[new_hash]], hashes_lst
--- attacks/preimage_attack.py
+++ attacks/preimage_attack.py
@@ -1,37 +1,62 @@
 import random
 import string
 
+chars = list(string.printable)
+chars.remove(',')
+chars.remove('\x0b')
+chars.remove('\x0c')
+
 # Послідовно додаємо до повідомлення натуральні числа
 def pa1(message, message_hash, hash_func, size=4):
+    messages, hashes = [], []
 
     if len(message_hash) < size:
         return False
     short_message_hash = message_hash[-size:]
 
     number = 1
-    new_hash = hash_func(message + str(number))[-size:]
+    messages.append(message + str(number))
+    nhash = hash_func(message + str(number))
+    hashes.append(nhash)
+    new_hash = nhash[-size:]
 
     while new_hash != short_message_hash:
         number += 1
-        new_hash = hash_func(message + str(number))[-size:]
+        messages.append(message + str(number))
+        nhash = hash_func(message + str(number))
+        hashes.append(nhash)
+        new_hash = nhash[-size:]
     
-    return number
+    return number, messages, hashes
 
 # Міняємо символ на випадковій позиції на новий, випадково обраний, символ
 def pa2(message, message_hash, hash_func, size=4):
+    messages, hashes = [], []
     if len(message_hash) < size:
         return False
     short_message_hash = message_hash[-size:]
 
     pos = random.randint(0, len(message)-1)
-    new_sym = random.choice(string.printable)
+    new_sym = random.choice(chars)
+
     message = message[:pos] + new_sym + message[pos+1:]
-    new_hash = hash_func(message)[-size:]
+    messages.append(message)
+    nhash = hash_func(message)
+    hashes.append(nhash)
+    new_hash = nhash[-size:]
+
+    count = 1
 
     while new_hash != short_message_hash:
         pos = random.randint(0, len(message)-1)
-        new_sym = random.choice(string.printable)
-        message = message[:pos] + new_sym + message[pos+1:]
-        new_hash = hash_func(message)[-size:]
+        new_sym = random.choice(chars)
 
-    return message
+        message = message[:pos] + new_sym + message[pos+1:]
+        messages.append(message)
+        nhash = hash_func(message)
+        hashes.append(nhash)
+        new_hash = nhash[-size:]
+
+        count += 1
+
+    return count, messages, hashes
